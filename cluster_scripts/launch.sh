b71a5bc9b16d2f5d5b85d47f99ae78236bc00079
toton-golio <qnf967@alumni.ku.dk>
2021-09-25 17:38:30 +0200

    cluster fixes

--- cluster_scripts/launch.sh
+++ cluster_scripts/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/results_NEW/"
+savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/results_paper/"
 
 L=14
 
-for i in `seq 0 999`
+for i in `seq 0 9`
 do
-	qsub -v output=$savedir,L=$L,S=$i, -N hubbard-L-$L-S-$i controlscript.pbs
+	qsub -v output_path=$savedir,L=$L,S=$i, -N hubbard-2NN-L-$L-S-$i controlscript.pbs
 done
