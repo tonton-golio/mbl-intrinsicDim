155369aed0df6e5c2aaf1e9bcba6ff72aa650b0b
tonton-golio <aggolles97@gmail.com>
2021-11-08 10:38:37 +0100

    a

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import*
+from utils import *
 import pandas as pd
 import seaborn as sns
 from plotting import *
