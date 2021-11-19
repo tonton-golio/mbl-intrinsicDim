c5587ef788e0efa9bdf566378d65218b320941cf
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2021-11-19 12:05:39 +0100

    Added batch launching versions

--- cluster_scripts/ID/launch.sh
+++ cluster_scripts/ID/launch.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/paper_data/"
 
+# Use batch for L=8, bc a single run over all Ws takes just a second
 L=8
-for i in `seq 0 2`
+for i in `seq 0 100 100000`
 do
-	qsub -l walltime=00:10:00,mem=1gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
+	Ss=$i
+        let Se=$i+100
+	qsub -l walltime=00:10:00,mem=1gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
 done
 
 L=10
-for i in `seq 0 2`
+for i in `seq 0 100 100000`
 do
-	qsub -l walltime=01:00:00,mem=2gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
+	Ss=$i
+        let Se=$i+100
+	qsub -l walltime=01:00:00,mem=2gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
 done
 
+# L = 12 takes ~5 min each, so we can also do multiple in 1 job
 L=12
-for i in `seq 0 2`
+for i in `seq 0 10 10000`
 do
-	qsub -l walltime=04:00:00,mem=4gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
+	Ss=$i
+        let Se=$i+10
+	qsub -l walltime=04:00:00,mem=4gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
 done
 
 L=14
---
+++ cluster_scripts/ID/run_batch.py
@@ -0,0 +1,32 @@
+# Add parent directory to path, so that we can find the utils_cluster module
+import os, sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from utils_cluster import *
+import argparse
+import numpy as np
+
+def run(L, startseed, endseed, output_path):
+    data = []
+    Ws = np.concatenate([np.arange(1,2.6,0.2), np.arange(2.6,4.55,0.05), np.arange(4.7,6.2,0.2)])
+
+    for seed in range(startseed, endseed):
+        for W in Ws:
+            H = constructHamiltonian(L = L, W = W, seed=seed)
+            _, eigvecs = np.linalg.eigh(H)
+            ID, rsquared, nndist = nn2(eigvecs)
+            data.append({'ID':ID, 'rsquared':rsquared, 'nndist':nndist})
+    
+        filename = output_path+'2nn_L_{0}_seed_{1}'.format(L, seed)+'.npy'
+        np.save(filename, np.array(data))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run 2NN on Hubbard chain Eigenvectors")
+    parser.add_argument('output_path', type=str, help='Output folder of the files')
+    parser.add_argument('-L', type=int, default=14, help='The system size (L)')
+    parser.add_argument('-Ss', type=int, help='The start seed number')
+    parser.add_argument('-Se', type=int, help='The end seed number')
+    args = parser.parse_args()
+
+    # Make sure the output_path directory exists
+    os.makedirs(args.output_path, exist_ok = True)
+    run(args.L, args.Ss, args.Se, args.output_path)
