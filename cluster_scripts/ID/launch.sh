8f8278dda0a4300b33926158c785b96f330ba3c0
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2021-12-09 13:47:47 +0100

    Update for L=16

--- cluster_scripts/ID/launch.sh
+++ cluster_scripts/ID/launch.sh
@@ -2,40 +2,47 @@
 savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/paper_data/"
 
 # Use batch for L=8, bc a single run over all Ws takes just a second
-L=8
-for i in `seq 0 1000 100000`
-do
-	Ss=$i
-        let Se=$i+1000
-	qsub -l walltime=00:10:00,mem=1gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
-done
+#L=8
+#for i in `seq 0 1000 100000`
+#do
+#	Ss=$i
+#        let Se=$i+1000
+#	qsub -l walltime=01:00:00,mem=1gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
+#done
 
-L=10
-for i in `seq 0 100 10000`
-do
-	Ss=$i
-        let Se=$i+100
-	qsub -l walltime=01:00:00,mem=2gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
-done
+#L=10
+#for i in `seq 0 100 10000`
+#do
+#	Ss=$i
+#        let Se=$i+100
+#	qsub -l walltime=02:00:00,mem=2gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
+#done
+#
 
 # L = 12 takes ~5 min each, so we can also do multiple in 1 job
-L=12
-for i in `seq 0 10 5000`
-do
-	Ss=$i
-        let Se=$i+10
-	qsub -l walltime=04:00:00,mem=4gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
-done
-#
+#L=12
+#for i in `seq 0 10 5000`
+#do
+#	Ss=$i
+#        let Se=$i+10
+#	qsub -l walltime=04:00:00,mem=4gb -v output_path=$savedir,L=$L,Ss=$Ss,Se=$Se -N 2NN-L-$L-S-$i controlscript_batch.pbs
+#done
+
 #L=14
-#for i in `seq 0 2`
+#for i in `seq 0 1000`
 #do
 #	qsub -l walltime=24:00:00,mem=8gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
 #done
 #
-#L=16
-#for i in `seq 0 2`
+L=16
+for i in `seq 2 30`
+do
+	qsub -l walltime=48:00:00,mem=32gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
+done
+
+#L=18
+#for i in 95 #`seq 0 100`
 #do
-#	qsub -l walltime=48:00:00,mem=10gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
+#	qsub -l walltime=48:00:00,mem=16gb -v output_path=$savedir,L=$L,S=$i -N 2NN-L-$L-S-$i controlscript.pbs
 #done
 #
--- cluster_scripts/ID/run.py
+++ cluster_scripts/ID/run.py
@@ -7,23 +7,27 @@ import numpy as np
 
 def run(L, seed, output_path):
     data = []
-    Ws = np.concatenate([np.arange(1,2.6,0.2), np.arange(2.6,4.55,0.05), np.arange(4.7,6.2,0.2)])
+    Ws =[1]# np.concatenate([np.arange(1,2.6,0.2), np.arange(2.6,4.55,0.05), np.arange(4.7,6.2,0.2)])
     for W in Ws:
+        print(W)
         H = constructHamiltonian(L = L, W = W, seed=seed)
         _, eigvecs = np.linalg.eigh(H)
         ID, rsquared, nndist = nn2(eigvecs)
         data.append({'ID':ID, 'rsquared':rsquared, 'nndist':nndist})
 
-    filename = output_path+'2nn_L_{0}_seed_{1}'.format(L, seed)+'.npy'
+    filename = output_path+'2nn_L_{0}_seed_{1}.npy'.format(L, seed)
     np.save(filename, np.array(data))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run 2NN on Hubbard chain Eigenvectors")
     parser.add_argument('output_path', type=str, help='Output folder of the files')
-    parser.add_argument('-L', type=int, default=14, help='The system size (L)')
+    parser.add_argument('-L', type=int, help='The system size (L)')
     parser.add_argument('-S', type=int, help='The seed number')
     args = parser.parse_args()
 
+    print("Running with L = {0} and seed = {1}".format(args.L,args.S))
+
     # Make sure the output_path directory exists
     os.makedirs(args.output_path, exist_ok = True)
+    print("Starting run")
     run(args.L, args.S, args.output_path)
