19f1818f1a2f137fff0145dc55f0652d9e2496b1
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2022-01-03 11:41:54 +0100

    Cluster script updates; rerun Plateau 16

--- cluster_scripts/ID/collect.sh
+++ cluster_scripts/ID/collect.sh
@@ -6,9 +6,10 @@ savedir="/home/projects/ku_00067/scratch/mbl-intrinsicdimension/paper_data/"
 module load tools
 module load anaconda3/2020.07
 
+cd $PBS_O_WORKDIR
+
 L=16
-for i in `seq 0 30`
+for i in `seq 0 999`
 do
-#	qsub -l walltime=48:00:00,mem=32gb -v output_path=$savedir,L=$L,S=$i -N collect-L-$L-S-$i controlscript_collect.pbs
 	python collect_for_W.py $savedir -L 16 -S $i
 done
